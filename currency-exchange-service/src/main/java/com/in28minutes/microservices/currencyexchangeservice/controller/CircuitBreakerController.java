2d6b122fc382852308ef82e41ea227f9bb7493c3
Roushan Kumar <rk04393@gmail.com>
2025-04-11 03:05:02 +0530

    here i seen how resilience ratelimiter and bulkhead works.

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,16 +19,21 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name = "default")
+    //In @RateLimiter we are tellig i 10s only sample 10000 calls.
+    @Bulkhead(name = "default")
     public String sampleApi() {
         logger.info("Sample api call received");
 
-        // Makes an HTTP GET request to another service (dummy URL for example)
-        ResponseEntity<String> forEntity = new RestTemplate()
-                .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        // Makes an HTTP GET request to another service (dummy URL for example)
+//        ResponseEntity<String> forEntity = new RestTemplate()
+//                .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//
+//        // Return the response body received from the external service
+//        return forEntity.getBody();
 
-        // Return the response body received from the external service
-        return forEntity.getBody();
+        return "sample-api";
     }
 
     // Fallback method to be called if retries are exhausted or an exception occurs
